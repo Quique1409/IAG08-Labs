35a733058ff68d9f88ed51a22ac264b0c77957ab
ericramirezz <eric.ram.val@gmail.com>
2025-09-08 10:05:09 -0600

    Final refactoring of functions for more clarity

--- Battlefield/GoalBasedAgentPlayer.py
+++ Battlefield/GoalBasedAgentPlayer.py
@@ -54,25 +54,30 @@ class GoalBasedAgentPlayer(IAPlayer):
             is_horizontal = first_hit[0] == second_hit[0] # Check if they are in the same row with the first value of the tuple
             
             line_targets = []
-            if is_horizontal:
+            if is_horizontal: #if two hits are in the same row, we use columns for move left and right
                 row = first_hit[0]
-                cols = [c for r,c in self.target_hits]
-                min_c, max_c = min(cols), max(cols)
-                if min_c - 1 >= 0: line_targets.append((row, min_c - 1))
-                if max_c + 1 < self.opponent_grid.size: line_targets.append((row, max_c + 1))
+                cols = [columns for rows,columns in self.target_hits] # Get all columns of the hits
+                min_c, max_c = min(cols), max(cols)     # Find the min and max column indices
+                if min_c - 1 >= 0: line_targets.append((row, min_c - 1)) # If the hit is not in a border cell, append it
+                if max_c + 1 < self.opponent_grid.size: line_targets.append((row, max_c + 1)) #add target to the right if is not in a border cell
             else: # Vertical
                 col = first_hit[1]
-                rows = [r for r,c in self.target_hits]
+                rows = [rws for rws,columns in self.target_hits] # Get all rows of the hits
                 min_r, max_r = min(rows), max(rows)
-                if min_r - 1 >= 0: line_targets.append((min_r - 1, col))
+                if min_r - 1 >= 0: line_targets.append((min_r - 1, col)) #we analize border cells of the rows from 0 - 9 for hit up and down
                 if max_r + 1 < self.opponent_grid.size: line_targets.append((max_r + 1, col))
 
-            valid_line_targets = [t for t in line_targets if self.opponent_grid.grid[t[0]][t[1]] == '.']
+            valid_line_targets = []
+
+            for t in line_targets: #from the possible targets in line, we check if they are valid
+                if self.opponent_grid.grid[t[0]][t[1]] == '.':
+                    valid_line_targets.append(t)
+                    
             if valid_line_targets:
-                return random.choice(valid_line_targets)
+                return random.choice(valid_line_targets) # Return a random valid target in line if available
 
         if neighbors_cells:
-            return random.choice(list(neighbors_cells))
+            return random.choice(list(neighbors_cells)) # If we dont have more than 1 hit, return a random neighbor cell (still in TARGET mode)
 
         return None # No valid targets found
     
