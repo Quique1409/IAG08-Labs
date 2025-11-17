61ba3fa7d1b4bdc1d9c16deed4f778aea3cf5a2f
luisgleonv <luisgleonv@gmail.com>
2025-11-17 13:07:03 -0600

    borre un temp file

--- SnakesAndStairs/tempCodeRunnerFile.py
+++ SnakesAndStairs/tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
-expected_moves
+def expected_moves(P):
+    """
+    Calculates the average numbers of moves to win
+    using the fundamental matrix N = (I - Q)^-1
+    """
+    # Q = Transtions between transient states (0-99 in our case)
+    transient_states = N_States - 1
+    Q = P[0:transient_states, 0:transient_states]
+    
+    # I = Identity matrix (same dimention as the number of transient states)
+    I = np.identity(transient_states)
