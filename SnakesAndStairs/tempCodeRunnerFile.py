4a301565ccc1186875ac857efc8dd74da5090b7a
Eric Ramirez <eric.ram.val@gmail.com>
2025-11-19 01:30:22 -0600

    final version

--- SnakesAndStairs/tempCodeRunnerFile.py
+++ SnakesAndStairs/tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-def iterative_prob_k_steps(M, v0, k):
-    v_k = v0.copy()
-    try:
-        start_time = time.time()
-        for step in range(k):
-            v_k = v_k @ M  # Matrix multiplication
-        end_time = time.time()
-        print(f"(Calculating v_k iteratively took {end_time - start_time:.4f}s)")
-        return v_k  # Return the vector of probabilities of being in all states after k moves
-    except np.linalg.LinAlgError as e:
-        print(f"Error obtaining v_{k} iteratively: {e}")
-        return None
+v0
